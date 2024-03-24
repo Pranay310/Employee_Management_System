fd11e62efaeb8b559391fbb864d8b77b3e288781
Pranay310 <146696037+Pranay310@users.noreply.github.com>
2024-03-24 10:57:05 +0530

    Uploading Features

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let selectedRow = null ;
+function openRegister(){
+	open("register.html")
+}
+function fetchData(){
+	let obj = {}
+	obj.name = document.getElementById('name').value;
+	obj.empId = document.getElementById('empId').value;
+	obj.address =  document.getElementById('address').value;
+	obj.designation = document.getElementById('designation').value;
+	
+	displayData(obj);
+}
+function displayData(ele){
+
+	let tbody = document.getElementById('table-body');
+	let newRow = tbody.insertRow(0);
+
+	let newCell1 = newRow.insertCell(0);
+	let newCell2 = newRow.insertCell(1);
+	let newCell3 = newRow.insertCell(2);
+	let newCell4 = newRow.insertCell(3);
+	newCell1.innerHTML = ele.name;
+	newCell2.innerHTML = ele.empId;
+	newCell3.innerHTML = ele.address;
+	newCell4.innerHTML = ele.designation;
+}
