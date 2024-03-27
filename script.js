b00c7bbbe54e031a7b4c0523b3db90e1c9cee664
Pranay310 <146696037+Pranay310@users.noreply.github.com>
2024-03-27 23:02:06 +0530

    Update

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function fetchData(){
 	obj.designation = document.getElementById('designation').value;
 	
 	displayData(obj);
+	clearFields(obj);
 }
 function displayData(ele){
 
@@ -21,8 +22,20 @@ function displayData(ele){
 	let newCell2 = newRow.insertCell(1);
 	let newCell3 = newRow.insertCell(2);
 	let newCell4 = newRow.insertCell(3);
+	let newCell5 = newRow.insertCell(4);
 	newCell1.innerHTML = ele.name;
 	newCell2.innerHTML = ele.empId;
 	newCell3.innerHTML = ele.address;
 	newCell4.innerHTML = ele.designation;
+	newCell5.innerHTML = `<button type="button" onclick="editData()">Edit</button>
+							<button type="button" onclick="deleteData()">Delete</button>`;
+}
+function clearFields(obj){
+	document.getElementById('name').value = "" ;
+	document.getElementById('empId').value = "" ;
+	document.getElementById('address').value = "" ;
+	document.getElementById('designation').value = "" ;
+}
+function editData(){
+
 }
